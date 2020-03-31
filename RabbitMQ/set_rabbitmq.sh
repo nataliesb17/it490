fce98721c67eec0830a17f3a2a5d9698a6dcab06
srcoleman97 <55894717+srcoleman97@users.noreply.github.com>
2020-03-30 23:22:11 -0400

    Delete set_rabbitmq.sh

--- RabbitMQ/set_rabbitmq.sh
+++
@@ -1,5 +0,0 @@
-#sets up rabbitMQ server with the software that it needs
-sudo apt-get upgrade -y
-sudo apt-get update -y
-
-sudo apt install -y php php-mbstring php-bcmath composer rsyslog git rabbitmq-server
