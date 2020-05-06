db12d9827e4c7f14063230f42acedd3d06ae93b5
Sophia Saint-Val <stval98@gmail.com>
2020-05-06 19:18:07 -0400

    test client

--- testPages/testRabbitMQClientSample.php
+++ testPages/testRabbitMQClientSample.php
@@ -7,22 +7,32 @@ error_reporting(E_ALL);
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
-require_once('functions.php');
 
 //have client send message
-$client = new RabbitMQClient('testRabbitMQ.ini', 'testServer');
+$request = "signin";
+$request = json_decode($request, true);
 
+$client = new RabbitMQClient('testRabbitMQ.ini', 'testServer');
+$arr = array("type" => "signin",
+"username" => "testuser",
+"password" => "testpass"
+);
+
+$response = $client->send_request($request);
+$response = json_encode($response);
+print_r($response);
+
+/*
 if(isset($argv[1])){
 	//$msg = $argv[1];
-	$msg = array("userInfo"=>$argv[1], "type"=> "signin");
+
 }
 else{
-	$msg = array("message"=>"test message", "type"=>"echo");
-	//$msg = array("query"=>"corona", "type"=>"get_news");
+
 }
 
 $response = $client->send_request($msg);
-
+*/
 echo "client received response: " . PHP_EOL;
 print_r($response);
 echo "\n\n";
