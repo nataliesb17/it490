9b3007c5030f569e75360215741344aefc150717
nataliesb17 <nataliesb17@gmail.com>
2020-05-11 01:48:03 +0000

    updated rabbitmq setup script

--- set_rabbitmq.sh
+++ set_rabbitmq.sh
@@ -2,4 +2,4 @@
 sudo apt-get upgrade -y
 sudo apt-get update -y
 
-sudo apt install -y php php-mbstring php-bcmath composer rsyslog git rabbitmq-server
+sudo apt install -y php php-mbstring php-bcmath composer rsyslog git
