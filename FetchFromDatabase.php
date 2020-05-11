9c9a976f56e6833ba7445a120fc0a733a72f80a3
neohazardous <ol29@njit.edu>
2020-05-10 23:09:01 -0400

    Updated audio levels

--- FetchFromDatabase.php
+++ FetchFromDatabase.php
@@ -1,14 +1,44 @@
 <?php
-$stmt = "select * from Pokemon where IDno = '$id'";
-$response = "";
 
-$table = mysqli_query($db, $stmt) or die(...);
-while ($row = mysqli_fetch_array($table, MYSQLI_ASSOC))
-    {
-        $name = $row['name']; 
-                $stat1 = $row['stat1'];
-                etc...
-                $reponse = "'$name','$stat1',....'$statX'"
-    }
-echo $reponse
+$dbhost = '';
+$dbuser = 'root';
+$dbpass = 'rootpswd';
+
+//connection str w/ database
+$conn = mysql_connect($dbhost, $dbuser, $dbpass);
+
+//error handling
+if(!$conn){
+	die("ERROR: Could not connect: " . mysql_error());
+}
+echo "Connected to database";
+
+//connect to database
+$select = mysql_select_db("pokedex", $dbhandle);
+or die("ERROR: Database not found");
+
+//queries
+$data = mysql_query("SELECT * FROM pokedex");
+$json_response = array();
+
+while($row = mysql_fetch_array($data, MYSQL_ASSOC)){
+	//fetch data in array format
+	$row_array['name'] = $row['name'];
+	array_push($json_response, $row_array);
+}
+
+echo json_encode($json_response);
+
+//$sql = 'SELECT * FROM pokedex WHERE name  ';
+
+/*
+$database_result = array();
+
+
+for (i = 1; i < 6; i++){
+
+	$slotname = "slot" + i.toString() //or whatever php uses
+}
+	
+echo $reponse*/
 ?>
