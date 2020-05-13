88a1a72d263c4552436cf453ad956f5287b82882
Sophia Saint-Val <stval98@gmail.com>
2020-05-12 21:30:31 -0400

    debug session variables -- profile

--- testPages/profile.php
+++ testPages/profile.php
@@ -1,27 +1,5 @@
 <?php 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-require_once('path.inc');
-require_once('get_host_info.inc');
-require_once('rabbitMQLib.inc');
-
 session_start();
-
-//have client send message
-$request = array("type" => "getUserInfo", "username" => $_SESSION["username"]);
-$req = json_decode($request, true);
-$client = new RabbitMQClient('testRabbitMQ.ini', 'testServer');
-$response = $client->send_request($request);
-$response = json_encode($response);
-print_r($response);
-echo "client received response: " . PHP_EOL;
-print_r($response);
-echo "\n\n";
-
-if(isset($argv[0]))
-echo $argv[0] . " END".PHP_EOL;
 ?>
 
 <!doctype html>
@@ -111,7 +89,8 @@ echo $argv[0] . " END".PHP_EOL;
                         <div class="col-lg-9"><p><?php print $_SESSION['name']; ?></p></div>
                       <label class="col-lg-3 col-form-label form-control-label">Username</label>
                         <div class="col-lg-9"><p><?php print $_SESSION['username']; ?></p></div>
-                        <label class="col-lg-3 col-form-label form-control-label"><?php echo $teams; ?></label>
+                      <label class="col-lg-3 col-form-label form-control-label">Teams</label>
+                      	<div class="col-lg-9"><p>Teams currently unavailable.</p></div>
                     </div>
                 </div>
 
