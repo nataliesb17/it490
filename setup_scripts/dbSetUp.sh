e9587f689eeb11caf651880aa05ae5343c202974
maniroopa <60149376+maniroopa@users.noreply.github.com>
2020-05-14 17:03:49 -0400

    Create dbSetUp.sh

---
+++ setup_scripts/dbSetUp.sh
@@ -0,0 +1,20 @@
+sudo apt-get upgrade -y
+
+sudo apt install git
+sudo apt install net-tools
+
+git clone https://github.com/nataliesb17/it490.git FantasyPokemon
+git clone https://github.com/MattToegel/IT490.git IT490Copy
+
+sudo apt-get install mysql-server
+mysql_secure_installation
+
+sudo apt install opens-server
+sudo systemctl status ssh
+sudo uff allow ssh
+
+sudo apt install php
+sudo apt install php-mbstring
+
+sudo apt install composer
+composer require php-amqplib/php-amqplib
